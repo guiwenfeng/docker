f33c80a087aacad428e1dc3d2a27ba84f4532ea5
guiwenfeng <943242261@qq.com>
2019-09-03 18:18:46 +0800

    dockerfile

---
+++ redis/docker-redis-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# first arg is `-f` or `--some-option`
+# or first arg is `something.conf`
+if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
+	set -- redis-server "$@"
+fi
+
+exec "$@"
